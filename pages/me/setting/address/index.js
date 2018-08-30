e73da2058a739baed3285dcc32c46d294e7bd733
A181A <2251234678@qq.com>
2018-08-30 15:08:32 +0800

    删除icons

--- pages/me/setting/address/index.js
+++ pages/me/setting/address/index.js
@@ -9,22 +9,33 @@ Page({
     addressId: null
   },
 
-  //选择地址
+
+  /**
+   * 确认订单页面选择地址
+   */
   selectAddress: function(e) {
-    const that = this;
-    var addressId = e.currentTarget.dataset.id;
-    var addressList = this.data.addressList;
-    for (let i = 0; i < addressList.length; i++) {
-      addressList[i].selected = false;
-      if (addressList[i].uId == addressId) {
-        addressList[i].selected = true;
+    const wxCurrPage = getCurrentPages(), //获取当前页面的页面栈
+      wxPrevPage = wxCurrPage[wxCurrPage.length - 2]; //获取上级页面的page对象
+
+    if (wxPrevPage.route == "pages/confirmOrder/index") {
+      var addressId = e.currentTarget.dataset.id,
+        addressList = this.data.addressList;
+
+      for (let i = 0, len = addressList.length; i < len; i++) {
+        addressList[i].selected = false;
+        if (addressList[i].uId == addressId) {
+          addressList[i].selected = true;
+        }
       }
+
+      this.setData({
+        addressList: addressList,
+        addressId: addressId
+      });
     }
-    this.setData({
-      addressList: addressList,
-      addressId: addressId
-    });
   },
+
+
   radioChange: function(e) {
     var addressList = this.data.addressList
     for (let i = 0; i < addressList.length; i++) {
@@ -50,12 +61,14 @@ Page({
         })
       });
   },
+
   editBind: function(e) {
     var address = this.data.addressList[e.currentTarget.dataset.index]
     wx.navigateTo({
       url: '../editaddress/index?address=' + JSON.stringify(address)
     })
   },
+
   delBind: function(e) {
     var that = this
     wx.showModal({
@@ -82,6 +95,45 @@ Page({
     })
 
   },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    var that = this,
+      header = getApp().globalData.header
+    getApp().requestGet('api/goods/queryAllUserAddress/0', null,
+      getApp().globalData.header,
+      function(res) {
+        var data = res.data.data
+        for (let i = 0, len = data.length; i < len; i++) {
+          data[i].address = data[i].address.replace(/,/g, ' ')
+          if (data[i].status == 1) {
+            data[i].checked = true
+          }
+          data[i].selected = false;
+        }
+        that.setData({
+          addressList: data
+        })
+      });
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+    const wxCurrPage = getCurrentPages(), //获取当前页面的页面栈
+      wxPrevPage = wxCurrPage[wxCurrPage.length - 2]; //获取上级页面的page对象
+    var addressId = this.data.addressId;
+    if (addressId !== null && wxPrevPage.route == "pages/confirmOrder/index") {
+      //修改确认页面的地址
+      wxPrevPage.setData({
+        addressId: addressId, //addressId为上级页面的某个数据
+      })
+    }
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -97,48 +149,10 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-    var that = this
-    var header = getApp().globalData.header
-    getApp().requestGet('api/goods/queryAllUserAddress/0', null,
-      getApp().globalData.header,
-      function(res) {
-        var data = res.data.data
-        for (let i = 0; i < data.length; i++) {
-          data[i].address = data[i].address.replace(/,/g, ' ')
-          if (data[i].status == 1) {
-            data[i].checked = true
-          }
-          data[i].selected = false;
-        }
-        that.setData({
-          addressList: data
-        })
-      });
-  },
-
-  /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function() {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-    const wxCurrPage = getCurrentPages(); //获取当前页面的页面栈
-    const wxPrevPage = wxCurrPage[wxCurrPage.length - 2]; //获取上级页面的page对象
-    var addressId = this.data.addressId;
-    if (addressId !== null && wxPrevPage.route == "pages/confirmOrder/index") {
-      //修改确认页面的地址
-      wxPrevPage.setData({
-        addressId: addressId, //addressId为上级页面的某个数据
-      })
-    }
   },
 
   /**
